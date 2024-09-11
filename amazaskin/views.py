353c17d854ab03078e2ca83dec0a7964766ec8c1
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2024-09-11 23:21:34 +0200

    added about section and remodelled landing page

--- amazaskin/views.py
+++ amazaskin/views.py
@@ -9,3 +9,11 @@ def landing(request):
 
     return render(request, 'amazaskin/landing.html', context)
 
+
+def about(request):
+
+    context = {
+        'page_title': 'Who We Are'
+    }
+
+    return render(request, 'amazaskin/about.html', context)
