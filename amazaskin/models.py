0f71b8d1e889c3ae73facb4efc9a801afe72b3d0
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2024-12-08 23:30:19 +0200

    created Product, Order, Customer and Category models

--- amazaskin/models.py
+++ amazaskin/models.py
@@ -1,3 +1,46 @@
+import datetime
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = PhoneNumberField(region='ZA')
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ImageField(upload_to='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255, default=name)
+
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=255)
+    phone_number = PhoneNumberField(region='ZA')
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
