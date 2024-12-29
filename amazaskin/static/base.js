30255f0a33f97194124c9721be4ba284adba1396
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2024-12-29 07:39:32 +0200

    added functionality to collapse nav if it's already open and the user scrolls

--- amazaskin/static/base.js
+++ amazaskin/static/base.js
@@ -4,3 +4,13 @@ const navMenu = document.getElementById("nav-menu");
 navMenu.addEventListener('click', () => {
     nav.classList.toggle("expanded");
 });
+
+// collapse nav if open
+window.addEventListener('scroll', () => {
+    if (nav.classList.contains('expanded')) {
+        const scrollThreshold = 400; // Adjust this value as needed
+        if (window.scrollY > scrollThreshold) {
+            nav.classList.remove('expanded');
+        }
+    }
+});
